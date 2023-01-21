0c272a319bb9eedddd1f7863e6ed11c12dd0ab15
slemankassis <slemankassis@hotmail.com>
2023-01-21 12:08:52 -0300

    Refactor and tests

--- dpk.js
+++ dpk.js
@@ -1,28 +1,37 @@
 const crypto = require("crypto");
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+const createHash = (algorithm) => (data) =>
+  crypto.createHash(algorithm).update(data).digest("hex");
+
+const createPartitionKey = (event) => {
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+  let partitionKey = event.partitionKey;
+  if (!partitionKey) {
+    const data = JSON.stringify(event);
+    partitionKey = createHash("sha3-512")(data);
+  }
+  return partitionKey;
+};
+
+const truncatePartitionKey = (partitionKey) => {
+  if (!partitionKey) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = createHash("sha3-512")(partitionKey);
+  }
+  return partitionKey;
+};
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  let partitionKey = createPartitionKey(event);
+  partitionKey = truncatePartitionKey(partitionKey);
+  return partitionKey;
 };
+
+exports.TRIVIAL_PARTITION_KEY = TRIVIAL_PARTITION_KEY;
+exports.MAX_PARTITION_KEY_LENGTH = MAX_PARTITION_KEY_LENGTH;
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,46 @@
-const { deterministicPartitionKey } = require("./dpk");
+const {
+  deterministicPartitionKey,
+  MAX_PARTITION_KEY_LENGTH,
+  TRIVIAL_PARTITION_KEY,
+} = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+    expect(trivialKey).toBe(TRIVIAL_PARTITION_KEY);
+  });
+
+  it("Should return a partition key based on the event data if partition key is not present", () => {
+    const event = {
+      data: { name: "some-name" },
+    };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toBeDefined();
+    expect(partitionKey.length).toBeLessThanOrEqual(MAX_PARTITION_KEY_LENGTH);
+  });
+
+  it("Should return the partition key when it is provided in the event object", () => {
+    const event = {
+      partitionKey: "some-key",
+    };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toBe(event.partitionKey);
+  });
+  it("Should return a truncated partition key when the original partition key is longer than the maximum length", () => {
+    const event = { partitionKey: "a".repeat(MAX_PARTITION_KEY_LENGTH + 1) };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey.length).toBeLessThanOrEqual(MAX_PARTITION_KEY_LENGTH);
+  });
+
+  it("Should return a string value for partition key", () => {
+    const event = { partitionKey: "some-key" };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(typeof partitionKey).toBe("string");
+  });
+
+  it("Should return a value for partition key even when the provided event is not in JSON format", () => {
+    const event = { name: "some-name", age: 20 };
+    const partitionKey = deterministicPartitionKey(event);
+    expect(partitionKey).toBeDefined();
   });
 });
